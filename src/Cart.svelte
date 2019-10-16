cd8b04b9580f6d8bade9ede99b4c2bfdc8262897
Tairone Livinalli <tairone.livinalli@meta.com.br>
2019-10-16 10:18:21 -0300

    Adicionar componente cart para o projeto

---
+++ src/Cart.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let items;
+
+    $: total = items.reduce((sum, currentValue) => {
+        return sum + currentValue.price;
+    }, 0);
+</script>
+<style>
+    ul {
+        list-style: none;
+        margin: 1rem;
+        padding: 0;
+    }
+
+    li {
+        margin: 0.5rem 0;
+        padding: 0.5rem;
+        border: 1px solid #ccc;
+    }
+
+    h1 {
+        font-size: 1.5rem;
+        margin: 1rem;
+    }
+</style>
+
+{#if items.length === 0}    
+    <p>No items in cart yet!</p>
+
+    {:else}
+    <ul>
+        {#each items as item} 
+            <li>{item.title} - ${item.price}</li>
+        {/each}
+    </ul>
+    <h1>TOTAL: ${total}</h1>
+{/if}
